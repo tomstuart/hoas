3457f405a54dc7a8872deb64be94d6a8e8b85910
Tom Stuart <tom@codon.com>
2017-03-18 21:31:32 +0000

    Redefine `stringify` with a fold

--- hoas.rb
+++ hoas.rb
@@ -1,9 +1,23 @@
 require 'rspec/expectations'
 include RSpec::Matchers
 
-Abs = Struct.new(:proc)
-App = Struct.new(:left, :right)
-Hole = Struct.new(:value)
+Abs = Struct.new(:proc) do
+  def fold(abs:, app:)
+    abs.(-> result { proc.(Hole.new(result)).fold(abs: abs, app: app) })
+  end
+end
+
+App = Struct.new(:left, :right) do
+  def fold(abs:, app:)
+    app.(left.fold(abs: abs, app: app), right.fold(abs: abs, app: app))
+  end
+end
+
+Hole = Struct.new(:value) do
+  def fold(abs:, app:)
+    value
+  end
+end
 
 def abs(*args)
   Abs.new(*args)
@@ -24,16 +38,12 @@ def all_names
   end
 end
 
-def stringify(term, names = all_names)
-  case term
-  when App
-    "#{stringify(term.left, names)} #{stringify(term.right, names)}"
-  when Abs
-    name = names.next
-    "(λ#{name}.#{stringify(term.proc.call(Hole.new(name)), names)})"
-  when Hole
-    term.value
-  end
+def stringify(term)
+  names = all_names
+
+  term.fold \
+    abs: -> f { x = names.next; "(λ#{x}.#{f.(x)})" },
+    app: -> l, r { "#{l} #{r}" }
 end
 
 RSpec::Matchers.define :look_like do |expected|
