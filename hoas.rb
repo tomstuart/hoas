a05f9d396e443a1f49cd444fa9acb6cee4b2fa60
Tom Stuart <tom@codon.com>
2017-03-18 21:31:54 +0000

    Define `eval_once`

--- hoas.rb
+++ hoas.rb
@@ -54,3 +54,23 @@ end
 
 omega = app(abs(-> x { app(x, x) }), abs(-> x { app(x, x) }))
 expect(omega).to look_like '(λa.a a) (λb.b b)'
+
+NoRuleApplies = Class.new(StandardError)
+
+def eval_once(term)
+  raise NoRuleApplies unless term.is_a?(App) && term.left.is_a?(Abs)
+  left, right = term.left, term.right
+
+  if right.is_a?(Abs)
+    left.proc.(right)
+  else
+    App.new(left, eval_once(right))
+  end
+end
+
+id = abs(-> x { x })
+term = app(id, app(id, abs(-> z { app(id, z) })))
+expect(term).to look_like '(λa.a) (λb.b) (λc.(λd.d) c)'
+expect(eval_once(term)).to look_like '(λa.a) (λb.(λc.c) b)'
+expect(eval_once(eval_once(term))).to look_like '(λa.(λb.b) a)'
+expect{eval_once(eval_once(eval_once(term)))}.to raise_error NoRuleApplies
