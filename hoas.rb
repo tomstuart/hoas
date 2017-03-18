6002755dec65c831ca55b95330ca6afca7615631
Tom Stuart <tom@codon.com>
2017-03-18 17:17:30 +0000

    Implement very basic higher-order abstract syntax
    The definition of `stringify` is awkward here — if we had static types,
    it wouldn’t work.
    
    We could get rid of this problem by switching to parametric higher-order
    abstract syntax.

---
+++ hoas.rb
@@ -0,0 +1,45 @@
+require 'rspec/expectations'
+include RSpec::Matchers
+
+Abs = Struct.new(:proc)
+App = Struct.new(:left, :right)
+
+def abs(*args)
+  Abs.new(*args)
+end
+
+def app(*args)
+  App.new(*args)
+end
+
+def all_names
+  Enumerator.new do |yielder|
+    name = 'a'
+
+    loop do
+      yielder.yield name
+      name = name.succ
+    end
+  end
+end
+
+def stringify(term, names = all_names)
+  case term
+  when App
+    "#{stringify(term.left, names)} #{stringify(term.right, names)}"
+  when Abs
+    name = names.next
+    "(λ#{name}.#{stringify(term.proc.call(name), names)})"
+  when String
+    term
+  end
+end
+
+RSpec::Matchers.define :look_like do |expected|
+  match do |actual|
+    stringify(actual) == expected
+  end
+end
+
+omega = app(abs(-> x { app(x, x) }), abs(-> x { app(x, x) }))
+expect(omega).to look_like '(λa.a a) (λb.b b)'
