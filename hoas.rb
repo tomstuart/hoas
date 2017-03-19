2a118bd21c8bc10a504e2449541bd9fef7b2ebef
Tom Stuart <tom@codon.com>
2017-03-19 11:47:47 +0000

    Build HOAS expressions with a parser
    This goes via an intermediate s-expression to make the transformation
    easier.

--- hoas.rb
+++ hoas.rb
@@ -1,6 +1,107 @@
 require 'rspec/expectations'
 include RSpec::Matchers
 
+class Parser
+  def initialize(builder)
+    self.builder = builder
+  end
+
+  def parse(string)
+    self.string = string
+    parse_everything
+  end
+
+  private
+
+  attr_accessor :builder
+  attr_reader :string
+
+  def string=(string)
+    @string = string.strip
+  end
+
+  def parse_everything
+    expression = parse_expression
+    read %r{\z}
+
+    expression
+  end
+
+  def parse_expression
+    parse_applications
+  end
+
+  def parse_applications
+    expression = parse_term
+
+    until can_read? %r{\)|\z} do
+      expression = builder.build_application(expression, parse_term)
+    end
+
+    expression
+  end
+
+  def parse_term
+    if can_read? %r{\(}
+      parse_brackets
+    elsif can_read? %r{[λ^\\]}
+      parse_abstraction
+    elsif can_read? %r{[a-z]+}
+      parse_variable
+    else
+      complain
+    end
+  end
+
+  def parse_brackets
+    read %r{\(}
+    expression = parse_expression
+    read %r{\)}
+
+    expression
+  end
+
+  def parse_abstraction
+    read %r{[λ^\\]}
+    parameter = read_name
+    read %r{\.}
+    body = parse_expression
+
+    builder.build_abstraction(parameter, body)
+  end
+
+  def parse_variable
+    name = read_name
+
+    builder.build_variable(name)
+  end
+
+  def read_name
+    read %r{[a-z]+}
+  end
+
+  def can_read?(pattern)
+    !try_match(pattern).nil?
+  end
+
+  def read(pattern)
+    match = try_match(pattern) || complain(pattern)
+    self.string = match.post_match
+    match.to_s
+  end
+
+  def try_match(pattern)
+    /\A#{pattern}/.match(string)
+  end
+
+  def complain(expected = nil)
+    complaint = "unexpected #{string.slice(0)}"
+    complaint << ", expected #{expected.inspect}" if expected
+
+    raise complaint
+  end
+end
+
 Abs = Struct.new(:proc) do
   def fold(abs:, app:)
     abs.(-> value { proc.(Result.new(value)).fold(abs: abs, app: app) })
@@ -19,12 +120,42 @@ Result = Struct.new(:value) do
   end
 end
 
-def abs(*args)
-  Abs.new(*args)
+class Builder
+  def build_abstraction(parameter, body)
+    [:abs, parameter, body]
+  end
+
+  def build_application(left, right)
+    [:app, left, right]
+  end
+
+  def build_variable(name)
+    [:var, name]
+  end
 end
 
-def app(*args)
-  App.new(*args)
+def make_hoas(sexp, env = {})
+  type, *args = sexp
+
+  case type
+  when :abs
+    parameter, body = args
+
+    Abs.new(-> x { make_hoas(body, env.merge(parameter => x)) })
+  when :app
+    left, right = args
+
+    App.new(make_hoas(left, env), make_hoas(right, env))
+  when :var
+    name, = args
+
+    env[name]
+  end
+end
+
+def parse(string)
+  sexp = Parser.new(Builder.new).parse(string)
+  make_hoas(sexp)
 end
 
 def all_names
@@ -45,7 +176,7 @@ RSpec::Matchers.define :look_like do |expected|
   end
 end
 
-omega = app(abs(-> x { app(x, x) }), abs(-> x { app(x, x) }))
+omega = parse '(λx.x x) λx.x x'
 expect(omega).to look_like '((λa.(a a)) (λb.(b b)))'
 
 NoRuleApplies = Class.new(StandardError)
@@ -61,8 +192,7 @@ def eval_once(term)
   end
 end
 
-id = abs(-> x { x })
-term = app(id, app(id, abs(-> z { app(id, z) })))
+term = parse '(λx.x) ((λx.x) λz.(λx.x) z)'
 expect(term).to look_like '((λa.a) ((λb.b) (λc.((λd.d) c))))'
 expect(eval_once(term)).to look_like '((λa.a) (λb.((λc.c) b)))'
 expect(eval_once(eval_once(term))).to look_like '(λa.((λb.b) a))'
