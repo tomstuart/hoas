d00161f7a2c1da7208767c743fe72f6166c3d514
Tom Stuart <tom@codon.com>
2017-03-19 21:01:42 +0000

    Extract SExp.alpha_equivalent?

--- hoas.rb
+++ hoas.rb
@@ -8,35 +8,9 @@ require 'sexp'
 include HOAS
 include HOAS::AST
 
-def alpha_equivalent?(a, b, a_env = {}, b_env = {})
-  a_type, *a_args = a
-  b_type, *b_args = b
-  return false unless a_type == b_type
-
-  case a_type
-  when :abs
-    a_parameter, a_body = a_args
-    b_parameter, b_body = b_args
-    variable = Object.new
-
-    alpha_equivalent?(a_body, b_body, a_env.merge(a_parameter => variable), b_env.merge(b_parameter => variable))
-  when :app
-    a_left, a_right = a_args
-    b_left, b_right = b_args
-
-    alpha_equivalent?(a_left, b_left, a_env, b_env) && alpha_equivalent?(a_right, b_right, a_env, b_env)
-  when :var
-    a_name, = a_args
-    b_name, = b_args
-
-    a_env.has_key?(a_name) == b_env.has_key?(b_name) &&
-      a_env.fetch(a_name, a_name) == b_env.fetch(b_name, b_name)
-  end
-end
-
 RSpec::Matchers.define :be_alpha_equivalent_to do |expected|
   match do |actual|
-    alpha_equivalent?(SExp.parse(expected), SExp.parse(actual))
+    SExp.alpha_equivalent?(SExp.parse(expected), SExp.parse(actual))
   end
 end
 
@@ -94,7 +68,7 @@ end
 
 RSpec::Matchers.define :be_the_term do |expected|
   match do |actual|
-    alpha_equivalent?(SExp.parse(stringify(actual)), SExp.parse(expected))
+    SExp.alpha_equivalent?(SExp.parse(stringify(actual)), SExp.parse(expected))
   end
 end
 
--- lib/sexp.rb
+++ lib/sexp.rb
@@ -5,4 +5,30 @@ module SExp
   def self.parse(string)
     Parser.new(Builder.new).parse(string)
   end
+
+  def self.alpha_equivalent?(a, b, a_env = {}, b_env = {})
+    a_type, *a_args = a
+    b_type, *b_args = b
+    return false unless a_type == b_type
+
+    case a_type
+    when :abs
+      a_parameter, a_body = a_args
+      b_parameter, b_body = b_args
+      variable = Object.new
+
+      alpha_equivalent?(a_body, b_body, a_env.merge(a_parameter => variable), b_env.merge(b_parameter => variable))
+    when :app
+      a_left, a_right = a_args
+      b_left, b_right = b_args
+
+      alpha_equivalent?(a_left, b_left, a_env, b_env) && alpha_equivalent?(a_right, b_right, a_env, b_env)
+    when :var
+      a_name, = a_args
+      b_name, = b_args
+
+      a_env.has_key?(a_name) == b_env.has_key?(b_name) &&
+        a_env.fetch(a_name, a_name) == b_env.fetch(b_name, b_name)
+    end
+  end
 end
