aed662f2c905437ac511e2a4e42fcf300e8bb532
Tom Stuart <tom@codon.com>
2017-03-21 21:18:04 +0000

    Extract alpha equivalence into a separate spec file

--- spec/hoas_spec.rb
+++ spec/hoas_spec.rb
@@ -1,28 +1,6 @@
 require 'hoas'
 require 'sexp'
 
-RSpec.describe 'alpha equivalence' do
-  RSpec::Matchers.define :be_alpha_equivalent_to do |expected|
-    match do |actual|
-      SExp.alpha_equivalent?(SExp.parse(expected), SExp.parse(actual))
-    end
-  end
-
-  specify { expect('λx.x').to be_alpha_equivalent_to 'λx.x' }
-  specify { expect('λx.x').not_to be_alpha_equivalent_to 'λy.x' }
-  specify { expect('λx.x').to be_alpha_equivalent_to 'λy.y' }
-  specify { expect('λx.x y').not_to be_alpha_equivalent_to 'λy.y y' }
-  specify { expect('λx.x y').not_to be_alpha_equivalent_to 'λy.y z' }
-  specify { expect('λx.x z').to be_alpha_equivalent_to 'λy.y z' }
-  specify { expect('λx.x z').not_to be_alpha_equivalent_to 'λx.x y' }
-  specify { expect('λx.λy.x y z').to be_alpha_equivalent_to 'λy.λx.y x z' }
-  specify { expect('(λx.x) λy.y').to be_alpha_equivalent_to '(λy.y) λx.x' }
-  specify { expect('(λx.x) λy.y').not_to be_alpha_equivalent_to '(λy.y) λx.y' }
-  specify { expect('λx.λy.λx.x y z').to be_alpha_equivalent_to 'λy.λy.λx.x y z' }
-  specify { expect('λx.λy.λx.x y z').to be_alpha_equivalent_to 'λy.λx.λy.y x z' }
-  specify { expect('λx.λy.λx.x y z').to be_alpha_equivalent_to 'λx.λx.λy.y x z' }
-end
-
 RSpec.describe 'pretty-printing' do
   RSpec::Matchers.define :look_like do |expected|
     match do |actual|
