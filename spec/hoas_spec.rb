27b717011a12d71cc4ab853d4e7ef2e55b038e73
Tom Stuart <tom@codon.com>
2017-03-21 21:28:45 +0000

    Show explicit HOAS construction in spec
    This is clumsy, but makes it easier to see what the HOAS representation
    actually looks like (separately from trying to understand the parser).

--- spec/hoas_spec.rb
+++ spec/hoas_spec.rb
@@ -1,6 +1,8 @@
 require 'hoas'
 require 'sexp'
 
+include HOAS::AST
+
 RSpec.describe 'pretty-printing' do
   RSpec::Matchers.define :look_like do |expected|
     match do |actual|
@@ -8,7 +10,12 @@ RSpec.describe 'pretty-printing' do
     end
   end
 
-  let(:omega) { HOAS.parse '(λx.x x) λx.x x' }
+  let(:omega) {
+    App.new(
+      Abs.new(-> x { App.new(x, x) }),
+      Abs.new(-> x { App.new(x, x) })
+    )
+  }
 
   specify { expect(omega).to look_like '((λa.(a a)) (λb.(b b)))' }
 end
@@ -20,10 +27,29 @@ RSpec.describe 'evaluation' do
     end
   end
 
-  let(:term) { HOAS.parse '(λx.x) ((λx.x) λz.(λx.x) z)' }
+  describe 'a simple term' do
+    let(:term) {
+      App.new(
+        Abs.new(-> x {
+          Abs.new(-> y {
+            App.new(x, y)
+          })
+        }),
+        Abs.new(-> z { z })
+      )
+    }
 
-  specify { expect(term).to be_the_term '(λx.x) ((λx.x) λz.(λx.x) z)' }
-  specify { expect(HOAS.eval_once(term)).to be_the_term '(λx.x) λz.(λx.x) z' }
-  specify { expect(HOAS.eval_once(HOAS.eval_once(term))).to be_the_term 'λz.(λx.x) z' }
-  specify { expect{HOAS.eval_once(HOAS.eval_once(HOAS.eval_once(term)))}.to raise_error HOAS::NoRuleApplies }
+    specify { expect(term).to be_the_term '(λx.λy.x y) λz.z' }
+    specify { expect(HOAS.eval_once(term)).to be_the_term 'λy.(λz.z) y' }
+    specify { expect { HOAS.eval_once(HOAS.eval_once(term)) }.to raise_error HOAS::NoRuleApplies }
+  end
+
+  describe 'a more complicated term' do
+    let(:term) { HOAS.parse '(λx.x) ((λx.x) λz.(λx.x) z)' }
+
+    specify { expect(term).to be_the_term '(λx.x) ((λx.x) λz.(λx.x) z)' }
+    specify { expect(HOAS.eval_once(term)).to be_the_term '(λx.x) λz.(λx.x) z' }
+    specify { expect(HOAS.eval_once(HOAS.eval_once(term))).to be_the_term 'λz.(λx.x) z' }
+    specify { expect { HOAS.eval_once(HOAS.eval_once(HOAS.eval_once(term))) }.to raise_error HOAS::NoRuleApplies }
+  end
 end
